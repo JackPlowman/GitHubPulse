4dc9cf2d1ebfe61513599b767ace75cab738272f
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-13 22:02:07 +0000

    chore: Init Swift Project (#1)
    # Pull Request
    
    ## Description
    
    This pull request includes the initial setup for the GitHubPulse
    project, including the main application, content view, assets, and basic
    test structures.
    
    ### Project Setup:
    *
    [`GitHubPulseApp.swift`](diffhunk://#diff-b9dcef24c31c2354f24cde7b1d8f108c4f99e0bcef4500ce400b8ad292cced4dR1-R17):
    Created the main application file with a basic SwiftUI app structure.
    *
    [`ContentView.swift`](diffhunk://#diff-bb6a2edac1c9e2e3fb6fb43b49ad1ce776d7d73dc4d3eac2bc975a31caacfdfaR1-R24):
    Added a simple content view displaying a globe icon and "Hello, world!"
    text.
    
    ### Asset Management:
    *
    [`Assets.xcassets/AccentColor.colorset/Contents.json`](diffhunk://#diff-832f9d1700bb0c5084468de3c29f1bf51cbdaf4dbfcbbb0eeae346ec1aa5ee74R1-R11):
    Added configuration for the accent color.
    *
    [`Assets.xcassets/AppIcon.appiconset/Contents.json`](diffhunk://#diff-751d68a049edca34920d16314e32cd4d9e95245e3a8c70f1908f371215a694daR1-R58):
    Defined various icon sizes for macOS.
    *
    [`Assets.xcassets/Contents.json`](diffhunk://#diff-018052c1a205fc4cb6cef31979724e30ce32a5b9a6523072cdd1ce7709c80223R1-R6):
    Added metadata for the asset catalog.
    * [`Preview Content/Preview
    Assets.xcassets/Contents.json`](diffhunk://#diff-018052c1a205fc4cb6cef31979724e30ce32a5b9a6523072cdd1ce7709c80223R1-R6):
    Added metadata for preview assets.
    
    ### Testing:
    *
    [`GitHubPulseTests.swift`](diffhunk://#diff-3b1af8cf5f11e5346f6abad17af02f3dc299b705ee38b52f4247da3a975e7328R1-R17):
    Created a basic structure for unit tests using the Testing framework.
    *
    [`GitHubPulseUITests.swift`](diffhunk://#diff-60bf287696e2e795de88e5da77db0e093dd01e3d7ee5e8af200aa1cfeb3e899fR1-R43):
    Set up UI tests with example test methods for launching the app and
    measuring performance.
    *
    [`GitHubPulseUITestsLaunchTests.swift`](diffhunk://#diff-c6ecf4ed09c2ae6cde0471607a10a337f5fc541783c6c17e35fc82b123538c8bR1-R33):
    Added UI launch tests to verify the app's launch behavior and capture
    screenshots.
    
    Fixes #2

---
+++ GitHubPulse/GitHubPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPulseApp.swift
+//  GitHubPulse
+//
+//  Created by Jack Plowman on 10/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
