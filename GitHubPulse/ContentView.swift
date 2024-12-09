00f23df54bff928a59bc61e5a8884933465b5016
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-09 21:05:44 +0000

    update

--- GitHubPulse/ContentView.swift
+++ GitHubPulse/ContentView.swift
@@ -6,20 +6,85 @@
 //
 
 import SwiftUI
+import Foundation
+
+class RSSFeedViewModel: ObservableObject {
+    @Published var feedItems: [String] = []
+
+    func fetchFeed() {
+        guard let url = URL(string: "https://github.blog/changelog/feed/") else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                return
+            }
+
+            if let data = data {
+                let parser = XMLParser(data: data)
+                let rssParserDelegate = RSSParserDelegate()
+                parser.delegate = rssParserDelegate
+
+                if parser.parse() {
+                    DispatchQueue.main.async {
+                        self.feedItems = rssParserDelegate.items
+                    }
+                }
+            }
+        }.resume()
+    }
+}
+
+class RSSParserDelegate: NSObject, XMLParserDelegate {
+    var items: [String] = []
+    private var currentElement = ""
+    private var currentTitle = ""
+
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
+        if currentElement == "item" {
+            currentTitle = ""
+        }
+    }
+
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        if currentElement == "title" {
+            currentTitle += string
+        }
+    }
+
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            items.append(currentTitle)
+        }
+    }
+
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        // Error handling can be implemented here if needed
+    }
+
+    func parser(_ parser: XMLParser, validationErrorOccurred validationError: Error) {
+        // Error handling can be implemented here if needed
+    }
+}
 
 struct RSSFeedView: View {
-    @State private var feedItems: [String] = ["Item 1", "Item 2", "Item 3"]
+    @ObservedObject var viewModel: RSSFeedViewModel
 
     var body: some View {
         NavigationView {
-            List(feedItems, id: \.self) { item in
+            List(viewModel.feedItems, id: \.self) { item in
                 Text(item)
             }
             .navigationTitle("GitHub Pulse")
+            .onAppear {
+                viewModel.fetchFeed()
+            }
         }
     }
 }
 
 #Preview {
-    RSSFeedView()
+    RSSFeedView(viewModel: RSSFeedViewModel())
 }
